2bc9777a4ae40794ec9b4e16402beae2e9c559b8
Jalaljalili <120138931+Jalaljalili@users.noreply.github.com>
2025-12-02 15:59:57 +0330

    Add rc_model.py

--- src/rc_model.py
+++ src/rc_model.py
@@ -1,4 +1,33 @@
-from scipy import signal
+from utils import transfer_function
 
-def get_rc_transfer_function(R, C):
-    return signal.TransferFunction([1], [C, 1/R])
+
+class LowPassRC:
+    """
+    RC low-pass filter
+    H(s) = 1 / (RC s + 1)
+    """
+
+    def __init__(self, R=1, C=1e-3):
+        self.R = R
+        self.C = C
+
+    def model(self):
+        num = [1]
+        den = [self.R * self.C, 1]
+        return transfer_function(num, den)
+
+
+class HighPassRC:
+    """
+    High-pass RC filter
+    H(s) = (RC s) / (RC s + 1)
+    """
+
+    def __init__(self, R=1, C=1e-3):
+        self.R = R
+        self.C = C
+
+    def model(self):
+        num = [self.R * self.C, 0]
+        den = [self.R * self.C, 1]
+        return transfer_function(num, den)
